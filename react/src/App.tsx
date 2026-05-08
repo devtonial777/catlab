37348971d3e5cbddfab0d171f85ccac4e3cd35cb
Marcos Tonial <devtonial@gmail.com>
2026-05-08 13:25:33 -0300

    feature/react - Startap do projeto inicial em react

---
+++ react/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className="bg-zinc-900 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        React + Tailwind funcionando 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
